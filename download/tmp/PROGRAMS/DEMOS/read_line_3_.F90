8f8c3e7d3c49f7e7c17046375eeae2ef0eb48622
init current directory[i] <urbanjost@comcast.net>
2019-12-25 12:48:49 -0500

    add second example to documenation and make read_line(3f) return error message, if any

--- download/tmp/PROGRAMS/DEMOS/read_line_3_.F90
+++ download/tmp/PROGRAMS/DEMOS/read_line_3_.F90
@@ -1,8 +1,24 @@
-          program demo_read_line
-          use M_io, only : read_line
-          implicit none
-          character(len=:),allocatable :: line
-             INFINITE: do while (read_line(line)==0)
-                write(*,'(a)')'['//line//']'
-             enddo INFINITE
-          end program demo_read_line
+           program demo_read_line
+           use,intrinsic :: iso_fortran_env, only : stdin  => input_unit
+           use,intrinsic :: iso_fortran_env, only : stdin  => input_unit
+           use,intrinsic :: iso_fortran_env, only : stderr => error_unit
+           use,intrinsic :: iso_fortran_env, only : iostat_end, iostat_eor
+           use M_io, only : read_line
+           implicit none
+           character (len =: ), allocatable :: line
+           integer  ::  ios, icount=0
+              INFINITE: do while (isave (read_line (line), ios) == 0)
+                 write (*, '(*(g0))') icount,' [',line,']'
+              enddo INFINITE
+              if (.not.is_iostat_end (ios).or..true.) then
+                 write (stderr, '(*(g0))') 'error: line ',icount,'==>',trim (line)
+              endif
+           contains
+              integer function isave (iin, iout)
+              integer, intent (in) :: iin
+              integer, intent (out) :: iout
+                 iout = iin
+                 isave = iin
+                 icount=icount+1
+              end function isave
+           end program demo_read_line
